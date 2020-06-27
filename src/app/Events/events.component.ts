182c85550a7afc9b9c29fee67d5a37b2dc29464b
Carlos Alexandre <carlos.nerware@gmail.com>
2020-06-27 17:42:09 -0300

    teste

---
+++ src/app/Events/events.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css'],
+})
+export class EventsComponent implements OnInit {
+  events: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
+  getEvents(){
+    this.events = this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {this.events = response; console.log(response)}, 
+      error =>{console.log(error);}
+      );
+  }
+}
